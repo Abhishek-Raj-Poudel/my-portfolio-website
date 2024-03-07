5220ffdac4d69b330b4f447165a67e29fabcc2d2
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-03-07 10:59:04 +0545

    implimented an express server

--- Static-Backup/tailwind.config.js
+++ Static-Backup/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        "darkest": "#121212",
+        "darkest2":"#222222",
+        "body":"#4C566A"
+      },
       fontFamily: {
         primary: ["JetBrains Mono"]
       }
